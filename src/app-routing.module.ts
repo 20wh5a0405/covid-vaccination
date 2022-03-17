96125482726fb8500181745557b1ec5083ab9c91
B Keerthi <19wh1a1208@bvrithyderabad.edu.in>
2022-03-17 06:06:12 +0000

    Upload New File

---
+++ src/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
+
+
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {path: 'home',component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'register', component: RegisterComponent }
+
+ 
+    
+  
+ 
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
